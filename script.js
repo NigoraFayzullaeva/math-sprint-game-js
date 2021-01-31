33cfcca4a976e19ddd18a3b1a25cd1eb14e892d6
Nigora <61627731+NigoraFayzullaeva@users.noreply.github.com>
2021-01-31 16:06:28 +0300

    countdown added

--- script.js
+++ script.js
@@ -83,6 +83,25 @@ function createEquations() {
 //   itemContainer.appendChild(bottomSpacer);
 // }
 
+function countdownStart() {
+  countdown.textContent = '3';
+  setTimeout(() => {
+    countdown.textContent = '2';
+  }, 1000);
+  setTimeout(() => {
+    countdown.textContent = '1';
+  }, 2000);
+  setTimeout(() => {
+    countdown.textContent = 'GO!';
+  }, 3000);
+}
+
+function showCountdown(){
+  countdownPage.hidden = false;
+  splashPage.hidden = true;
+  countdownStart();
+}
+
 function getRadioValue(){
   let radioValue;
   radioInputs.forEach((radioInput) => {
@@ -96,6 +115,9 @@ function getRadioValue(){
 function selectQuestionAmount(e){
   e.preventDefault();
   questionAmount = getRadioValue();
+  if (questionAmount){
+    showCountdown();
+  }
 }
 
 startForm.addEventListener('click', () => {
