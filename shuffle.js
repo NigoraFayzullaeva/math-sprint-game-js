f81b9dcd4ffe18b0a33dea4a825054a0153162fb
Nigora <61627731+NigoraFayzullaeva@users.noreply.github.com>
2021-02-01 09:13:38 +0300

    final➕

--- shuffle.js
+++ shuffle.js
@@ -1,24 +1,19 @@
 function shuffle(array) {
-    var currentIndex = array.length,
-        temporaryValue, randomIndex;
+  let currentIndex = array.length;
+  let temporaryValue;
+  let randomIndex;
 
-    // While there remain elements to shuffle...
-    while (0 !== currentIndex) {
+  // While there remain elements to shuffle...
+  while (currentIndex !== 0) {
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
 
-        // Pick a remaining element...
-        randomIndex = Math.floor(Math.random() * currentIndex);
-        currentIndex -= 1;
+    // And swap it with the current element.
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
 
-        // And swap it with the current element.
-        temporaryValue = array[currentIndex];
-        array[currentIndex] = array[randomIndex];
-        array[randomIndex] = temporaryValue;
-    }
-
-    return array;
+  return array;
 }
-
-// Used like so
-var arr = [2, 11, 37, 42];
-shuffle(arr);
-console.log(arr);
